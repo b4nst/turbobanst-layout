52a0377d850064f89b380993f8397123c3ce0946
Bastien Arata <bastien.arata@d2si.io>
2019-04-02 16:46:09 +0200

    :zap: improve perf

--- data-analysis/src/char-process-stream.js
+++ data-analysis/src/char-process-stream.js
@@ -1,36 +1,65 @@
 const stream = require("stream");
+const _ = require("lodash");
+
+const FLUSH_EVERY = 100000;
+
+const souldCharBeCounted = _.memoize(char => !/\s/.test(char));
 
 class CharProcessStream extends stream.Writable {
   constructor(redis, options = {}) {
     options.objectMode = true;
     super(options);
     this.redis = redis;
+    this.pipeline = redis.pipeline();
     this.previousChar = "SoF";
   }
 
   _write(char, enconding, cb) {
     var pipeline = this.redis.pipeline();
-    this._pushCharToPipeline(char, pipeline).exec(cb);
+    this._pushCharToPipeline(char);
+    this._done(cb);
   }
 
   _writev(chunks, cb) {
-    chunks
-      .map(c => c.chunk)
-      .reduce(
-        (acc, curr) => this._pushCharToPipeline(curr, acc),
-        this.redis.pipeline()
-      )
-      .exec(cb);
+    chunks.map(c => c.chunk).forEach(char => this._pushCharToPipeline(char));
+    this._done(cb);
   }
 
-  _pushCharToPipeline(char, pipeline) {
-    var result = pipeline
-      .zincrby("charcount", 1, char)
-      .zincrby("charcount", 1, "total")
-      .zincrby(`bigram:${char}`, 1, this.previousChar)
-      .zincrby(`bigram:${this.previousChar}`, 1, char);
+  _final(cb) {
+    this._flush()
+      .then(cb)
+      .catch(cb);
+  }
+
+  _pushCharToPipeline(char) {
+    if (souldCharBeCounted(char)) {
+      this.pipeline
+        .zincrby("charcount", 1, "total")
+        .zincrby("charcount", 1, char);
+
+      if (souldCharBeCounted(this.previousChar)) {
+        const bigram =
+          this.previousChar > char
+            ? `${this.previousChar}:${char}`
+            : `${char}:${this.previousChar}`;
+        this.pipeline.zincrby("bigram", 1, bigram);
+      }
+    }
     this.previousChar = char;
-    return result;
+  }
+
+  async _flush() {
+    await this.pipeline.exec();
+    this.pipeline = this.redis.pipeline();
+    return null;
+  }
+
+  _done(cb) {
+    if (this.pipeline.length < FLUSH_EVERY) cb(null);
+    else
+      this._flush()
+        .then(cb)
+        .catch(cb);
   }
 }
 
--- data-analysis/src/file-processor.js
+++ data-analysis/src/file-processor.js
@@ -1,36 +1,25 @@
-const stream = require('stream');
-const { Map } = require('warp-pipes');
-const fs = require('fs');
+const stream = require("stream");
+const fs = require("fs");
 
-const CharProcessStream = require('./char-process-stream');
-const StreamToChar = require('./stream-to-char');
-
-const replaceBlank = char => {
-    switch (char) {
-        case ' ' : return 'space'
-        case '\n' : return 'cr'
-        case '\t' : return 'tab'
-        default: return char;
-    }
-}
+const CharProcessStream = require("./char-process-stream");
+const StreamToChar = require("./stream-to-char");
 
 class FileProcessor extends stream.Writable {
   constructor(redis, options = {}) {
     options.objectMode = true;
     super(options);
-    this.redis = redis
+    this.redis = redis;
   }
 
   _write(entryInfo, encoding, callback) {
     fs.createReadStream(entryInfo.fullPath)
       .pipe(new StreamToChar())
-      .pipe(new Map(replaceBlank, { objectMode: true })) // replace blank char
       .pipe(new CharProcessStream(this.redis))
-      .on('error', callback)
-      .on('finish', () => {
-          console.log('Done with file :', entryInfo.path)
-          callback();
-        });
+      .on("error", callback)
+      .on("finish", () => {
+        console.log("Done with file :", entryInfo.path);
+        callback();
+      });
   }
 }
 
